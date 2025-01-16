a7dbf83f51e98d94ac188273175d6787b0f13c34
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-01-16 15:38:51 +0600

    add react-query for data fetching; wrap application in QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Routes";
 import AuthProvider from "./providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+// Create a client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );

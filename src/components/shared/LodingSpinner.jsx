ceda8351b02d3ae808ec96602d9b67b886075816
Abdur Rahman Mahmud <ar7543078@gmail.com>
2025-01-20 15:55:33 +0600

    feat: enhance LoadingSpinner and AllTrainer components with animations and SEO improvements

--- src/components/shared/LodingSpinner.jsx
+++ src/components/shared/LodingSpinner.jsx
@@ -1,8 +1,38 @@
 /* eslint-disable react/prop-types */
 
 import { ScaleLoader } from 'react-spinners'
-
-const LoadingSpinner = ({ smallHeight=0 }) => {
+import { motion } from 'framer-motion';
+import logo from '../../assets/logo.svg'
+const LoadingSpinner = ({ smallHeight=0 }) => 
+  {
+    const loadingContainerVariants = {
+      start: {
+        transition: {
+          staggerChildren: 0.2,
+        },
+      },
+      end: {
+        transition: {
+          staggerChildren: 0.2,
+        },
+      },
+    };
+  
+    const loadingCircleVariants = {
+      start: {
+        y: "0%",
+      },
+      end: {
+        y: "100%",
+      },
+    };
+  
+    const loadingCircleTransition = {
+      duration: 0.5,
+      repeat: Infinity,
+      repeatType: "reverse",
+      ease: "easeInOut",
+    };
   return (
     <div
       className={` ${smallHeight ? 'h-[250px]' : 'h-[70vh]'}
@@ -11,7 +41,25 @@ const LoadingSpinner = ({ smallHeight=0 }) => {
       justify-center 
       items-center `}
     >
-      <ScaleLoader size={100} color='lime' />
+       <div className="flex justify-center items-center min-h-screen">
+        <motion.div
+          variants={loadingContainerVariants}
+          initial="start"
+          animate="end"
+          className="flex gap-2"
+        >
+          {[1, 2, 3].map((_, i) => (
+            <motion.img
+              key={i}
+              src={logo}
+              variants={loadingCircleVariants}
+              transition={loadingCircleTransition}
+              className="w-5 h-5 bg-gray-900 rounded-full"
+            />
+            
+          ))}
+        </motion.div>
+      </div>
     </div>
   )
 }
--- src/pages/AllTrainer.jsx
+++ src/pages/AllTrainer.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { useQuery } from "react-query";
 import useAxiosPublic from "../hooks/useAxiosPublic";
 import LoadingSpinner from "../components/shared/LodingSpinner";
+import { Helmet } from "react-helmet-async";
 
 const socialLinks = [
   {
@@ -35,69 +36,120 @@ const AllTrainer = () => {
     },
   });
   console.log(trainers);
+  // Animation variants
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const cardVariants = {
+    hidden: {
+      opacity: 0,
+      y: 50,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.6,
+        ease: "easeOut",
+      },
+    },
+  };
+
   if (isLoading) return <LoadingSpinner />;
   return (
-    <section className="py-12 ">
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <Typography
-          variant="h2"
-          className="text-center mb-12 font-bold text-orange-700"
+    <>
+      <Helmet>
+        <title>FitTrack - Meet Our Trainers </title>
+        <meta
+          name="description"
+          content="Meet our experienced fitness trainers who will help you achieve your fitness goals"
+        />
+      </Helmet>
+      <section className="py-12 ">
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
         >
-          Meet Our Trainers
-        </Typography>
-        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8">
-          {trainers?.length > 0
-            ? trainers?.map((trainer = {}) => (
-                <Card
+          <Typography
+            variant="h2"
+            className="text-center mb-12 font-bold text-orange-700"
+          >
+            Meet Our Trainers
+          </Typography>
+          <motion.div
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+            className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8"
+          >
+            {trainers?.length > 0 ? (
+              trainers?.map((trainer = {}) => (
+                <motion.div
                   key={trainer._id}
-                  className="p-6 ring ring-orange-900 bg-black shadow-lg  text-white flex flex-col items-center"
+                  variants={cardVariants}
+                  viewport={{ once: true }}
                 >
-                  <Avatar
-                    src={trainer.profileImage}
-                    alt={trainer.fullName}
-                    size="xl"
-                    className="mb-4"
-                  />
-                  <Typography
-                    variant="h4"
-                    color="blue-gray"
-                    className="mb-2 font-semibold text-center text-gray-200"
-                  >
-                    {trainer.fullName}
-                  </Typography>
-                  <Typography className="text-gray-400 text-sm text-center mb-4">
-                    {trainer.experience} Years Of Experience
-                  </Typography>
-                  <div className="flex gap-3 text-xl">
-                    {socialLinks.map(({ icon, url }, idx) => (
-                      <a
-                        href={url}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        key={idx}
-                      >
-                        {icon}
-                      </a>
-                    ))}
-                  </div>
-                  <Button
-                    color="orange"
-                    variant="filled"
-                    className="mt-4"
-                    onClick={() => navigate(`/trainer/${trainer._id}`)}
-                  >
-                    Know More
-                  </Button>
-                </Card>
+                  <Card className="p-6 ring ring-orange-900 bg-black shadow-lg  text-white flex flex-col items-center">
+                    <Avatar
+                      src={trainer.profileImage}
+                      alt={trainer.fullName}
+                      size="xl"
+                      className="mb-4"
+                    />
+                    <Typography
+                      variant="h4"
+                      color="blue-gray"
+                      className="mb-2 font-semibold text-center text-gray-200"
+                    >
+                      {trainer.fullName}
+                    </Typography>
+                    <Typography className="text-gray-400 text-sm text-center mb-4">
+                      {trainer.experience} Years Of Experience
+                    </Typography>
+                    <div className="flex gap-3 text-xl">
+                      {socialLinks.map(({ icon, url }, idx) => (
+                        <a
+                          href={url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          key={idx}
+                        >
+                          {icon}
+                        </a>
+                      ))}
+                    </div>
+                    <Button
+                      color="orange"
+                      variant="filled"
+                      className="mt-4"
+                      onClick={() => navigate(`/trainer/${trainer._id}`)}
+                    >
+                      Know More
+                    </Button>
+                  </Card>
+                </motion.div>
               ))
-            : <div className="text-white text-center col-span-full text-2xl">No Trainers Found</div>}
-        </div>
-      </motion.div>
-    </section>
+            ) : (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                className="text-white text-center col-span-full text-2xl"
+              >
+                No Trainers Found
+              </motion.div>
+            )}
+          </motion.div>
+        </motion.div>
+      </section>
+    </>
   );
 };
 
